1f653dae6bf70ba296ea6ab76c061ab7f91fcf40
AllenWang <allenwang950321@gmail.com>
2017-07-02 00:35:07 -0700

    arranged all the header file and related source file

---
+++ CS32_Project01/CS32_Project01/Game.h
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include"globals.h"
+#include "Arena.h"
+#include "Player.h"
+
+
+class Arena;
+class Player;
+
+class Game
+{
+public:
+	// Constructor/destructor
+	Game(int rows, int cols, int nRobots);
+	~Game();
+
+	// Mutators
+	void play();
+
+private:
+	Arena* m_arena;
+};
+
+
+#endif 
+
